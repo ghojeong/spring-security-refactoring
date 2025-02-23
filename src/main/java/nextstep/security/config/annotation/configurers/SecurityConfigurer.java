4954e95203b1637f21734c842d494c42bad76dad
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2025-02-23 09:16:55 +0900

    feat: SecurityConfigurer 인터페이스를 선언하고, SecurityContextConfigurer 를 구현

---
+++ src/main/java/nextstep/security/config/annotation/configurers/SecurityConfigurer.java
@@ -0,0 +1,8 @@
+package nextstep.security.config.annotation.configurers;
+
+import nextstep.security.config.annotation.HttpSecurity;
+
+// NOTE: https://github.com/spring-projects/spring-security/blob/main/config/src/main/java/org/springframework/security/config/annotation/SecurityConfigurer.java
+public interface SecurityConfigurer {
+    void configure(HttpSecurity http);
+}
